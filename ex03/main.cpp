a00c66922401ed8efd354042bdedd3b24a28e96e
kjroy93 <kmarrero1993@gmail.com>
2026-05-27 20:30:09 +0200

    ex03 done

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kjroydev <kjroydev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 18:40:06 by kjroydev          #+#    #+#             */
-/*   Updated: 2026/05/26 18:40:07 by kjroydev         ###   ########.fr       */
+/*   Updated: 2026/05/27 20:29:04 by kjroydev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@
 int main()
 {
 	{
-	Weapon club = Weapon("crude spiked club");
+	Weapon club = Weapon(" crude spiked club");
 	HumanA bob("Bob", club);
 	bob.attack();
-	club.setType("some other type of club");
+	club.setType(" some other type of club");
 	bob.attack();
 	}
 	{
 	}
-	Weapon club = Weapon("crude spiked club");
+	Weapon club = Weapon(" crude spiked club");
 	HumanB jim("Jim");
 	jim.setWeapon(club);
 	jim.attack();
-	club.setType("some other type of club");
+	club.setType(" some other type of club");
 	jim.attack();
 	return 0;
 }
